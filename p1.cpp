52180a61530e0b47d0dfe3cf58f256adbee0a244
henrydl <henrydl@umich.edu>
2015-06-01 09:07:09 -0400

    First Euler

---
+++ p1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+int count(int top){
+  int total = 0;
+  for(int x = 0; x < top; x++){
+    if(x%3 == 0 || x%5 == 0){
+      total += x;
+    }
+  }
+  return total;
+}
+      
+int main(){
+  //cout << count(1000) << endl;
+  assert(count(10) == 22);
+  return 1;
+}
+
